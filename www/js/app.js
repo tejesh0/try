1ecc7c55dd650605255b6ac0cd3b481684386383
Sasi Kumar <sasikumar@SZLAP425.(none)>
2015-06-28 10:54:26 +0530

    polo

--- www/js/app.js
+++ www/js/app.js
@@ -25,34 +25,34 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services',  
 
   // $translate.use(lang);
   // $translateProvider.preferredLanguage('hi');
-  $translateProvider
-    // cs: language locales from lang/*.json by static loader
-   .useStaticFilesLoader({
-    prefix: 'lang/locale-',
-    suffix: '.json'
-    })
+  // $translateProvider
+  //   // cs: language locales from lang/*.json by static loader
+  //  .useStaticFilesLoader({
+  //   prefix: 'lang/locale-',
+  //   suffix: '.json'
+  //   })
 
-    // cs:default language i language for missing translations
-        .preferredLanguage('hi') // cs:preferred language
-    // cs:language used for missing translation, may be a list of languages: ids separated with comma
-  .fallbackLanguage('en') 
+  //   // cs:default language i language for missing translations
+  //       .preferredLanguage('hi') // cs:preferred language
+  //   // cs:language used for missing translation, may be a list of languages: ids separated with comma
+  // .fallbackLanguage('en') 
 
-    // cs:register my available languages
-  .registerAvailableLanguageKeys(['pl', 'en', 'de', 'it', 'es', 'fr', 'pt'], {
-  'pl' : 'pl', 'pl_PL': 'pl',
-  'en' : 'en', 'en_GB': 'en', 'en_US': 'en', 'en_AU': 'en', 'en_CA': 'en', 'en_IN': 'en', 'en_IE': 'en', 'en_MT': 'en', 'en_NZ': 'en', 'en_PH': 'en', 'en_SG': 'en', 'en_ZA': 'en', 'en_UK': 'en',
-  'de' : 'de', 'de_DE': 'de', 'de_CH': 'de', 'de_AT': 'de', 'de_LU': 'de',
-  'it' : 'it', 'it_IT': 'it', 'it_CH': 'it',
-  'es' : 'es', 'es_ES': 'es', 'es_AR': 'es', 'es_BO': 'es', 'es_CL': 'es', 'es_CO': 'es', 'es_CR': 'es', 'es_DO': 'es', 'es_EC': 'es', 'es_SV': 'es', 'es_GT': 'es', 'es_HN': 'es', 'es_MX': 'es', 'es_NI': 'es', 'es_PA': 'es', 'es_PY': 'es', 'es_PE': 'es', 'es_PR': 'es', 'es_ES': 'es', 'es_US': 'es', 'es_UY': 'es', 'es_VE': 'es',
-  'fr' : 'fr', 'fr_BE': 'fr', 'fr_CA': 'fr', 'fr_FR': 'fr', 'fr_LU': 'fr', 'fr_CH': 'fr',
-  'pt' : 'pt', 'pt_BR': 'pt', 'pt_PT': 'pt'
-        })
+  //   // cs:register my available languages
+  // .registerAvailableLanguageKeys(['pl', 'en', 'de', 'it', 'es', 'fr', 'pt'], {
+  // 'pl' : 'pl', 'pl_PL': 'pl',
+  // 'en' : 'en', 'en_GB': 'en', 'en_US': 'en', 'en_AU': 'en', 'en_CA': 'en', 'en_IN': 'en', 'en_IE': 'en', 'en_MT': 'en', 'en_NZ': 'en', 'en_PH': 'en', 'en_SG': 'en', 'en_ZA': 'en', 'en_UK': 'en',
+  // 'de' : 'de', 'de_DE': 'de', 'de_CH': 'de', 'de_AT': 'de', 'de_LU': 'de',
+  // 'it' : 'it', 'it_IT': 'it', 'it_CH': 'it',
+  // 'es' : 'es', 'es_ES': 'es', 'es_AR': 'es', 'es_BO': 'es', 'es_CL': 'es', 'es_CO': 'es', 'es_CR': 'es', 'es_DO': 'es', 'es_EC': 'es', 'es_SV': 'es', 'es_GT': 'es', 'es_HN': 'es', 'es_MX': 'es', 'es_NI': 'es', 'es_PA': 'es', 'es_PY': 'es', 'es_PE': 'es', 'es_PR': 'es', 'es_ES': 'es', 'es_US': 'es', 'es_UY': 'es', 'es_VE': 'es',
+  // 'fr' : 'fr', 'fr_BE': 'fr', 'fr_CA': 'fr', 'fr_FR': 'fr', 'fr_LU': 'fr', 'fr_CH': 'fr',
+  // 'pt' : 'pt', 'pt_BR': 'pt', 'pt_PT': 'pt'
+  //       })
     
-    // cs: user language detection
-  .determinePreferredLanguage()
+  //   // cs: user language detection
+  // .determinePreferredLanguage()
     
-    // cs:remember language in custom storage 
-  .useLocalStorage();
+  //   // cs:remember language in custom storage 
+  // .useLocalStorage();
 
 
     // $translateProvider.translations('hi', {
